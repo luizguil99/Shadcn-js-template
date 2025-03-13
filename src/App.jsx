8957123520ab8142532add297217211a3feb5b1a
luizguil99 <124329344+luizguil99@users.noreply.github.com>
2025-03-12 21:21:36 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <Button>Botao de teste</Button>
+      
     </>
   )
 }
