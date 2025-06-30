ec1c06abc94ac9d6f8ec2166bee96f7b031a0f4d
luizguil99 <124329344+luizguil99@users.noreply.github.com>
2025-06-30 16:35:10 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { Button } from './components/ui/button'
+
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen bg-background flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Vite/React js-Template</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
